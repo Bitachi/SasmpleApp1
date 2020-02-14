46a38a47304715c28d33936ab729a013971bd644
Go Takami <funkykong888@gmail.com>
2020-02-14 11:21:05 +0900

    impremented user signup property

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,29 @@
 class UserController < ApplicationController
   def new
+    #debugger
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #保存のせいこうしょり
+      flash[:success] = "Welcome to the Sample App1!"
+      redirect_to @user
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+   def user_params
+     params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'user#new'
+  post "/signup", to: "user#create"
+  resources :user
 end
